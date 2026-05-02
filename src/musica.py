2dea4e08abab9ffc01530bf6bcc8aacfcb5894e1
vladimirsejas <vladimirqueirozsejas@gmail.com>
2026-05-02 14:09:12 -0300

    atualizando classe musica e exibicao de dados

--- src/musica.py
+++ src/musica.py
@@ -1,14 +1,19 @@
 class Musica:
-    _contador_id = 1
+    contador_id = 1
 
     def __init__(self, titulo, artista, genero, bpm):
-        self.id = Musica._contador_id
-        Musica._contador_id += 1
+        self.id = Musica.contador_id
+        Musica.contador_id = Musica.contador_id + 1
 
         self.titulo = titulo
         self.artista = artista
         self.genero = genero
         self.bpm = bpm
 
-    def __str__(self):
-        return f"[{self.id}] {self.titulo} - {self.artista} | {self.genero} | {self.bpm} BPM"
+    def exibir_dados(self):
+        print("ID:", self.id)
+        print("Título:", self.titulo)
+        print("Artista:", self.artista)
+        print("Gênero:", self.genero)
+        print("BPM:", self.bpm)
+        print("----------------------")
